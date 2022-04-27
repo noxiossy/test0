8f6f3563118d508f15e4ef9598c60b67bc341bc4
noxiossy <cari0us@outlook.com>
2022-04-27 20:28:11 +0700

    R

--- xray/xrGame/WeaponMagazinedWGrenade.cpp
+++ xray/xrGame/WeaponMagazinedWGrenade.cpp
@@ -144,7 +144,7 @@ void CWeaponMagazinedWGrenade::OnShot		()
 	if(m_bGrenadeMode)
 	{
 		PlayAnimShoot		();
-		PlaySound			("sndShotG", get_LastFP2(), true);
+		PlaySound			("sndShotG", get_LastFP2());
 		AddShotEffector		();
 		StartFlameParticles2();
 	} 
