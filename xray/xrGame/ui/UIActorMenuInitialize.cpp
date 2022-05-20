d3fd19b3d49616638e0db597aab74c58c33abd7c
noxiossy <cari0us@outlook.com>
2022-05-20 22:07:38 +0700

    sasa

--- xray/xrGame/ui/UIActorMenuInitialize.cpp
+++ xray/xrGame/ui/UIActorMenuInitialize.cpp
@@ -148,8 +148,8 @@ void CUIActorMenu::Construct()
 
 	m_trade_button		= UIHelper::Create3tButtonEx(uiXml, "trade_button", this);
 	m_takeall_button	= UIHelper::Create3tButtonEx(uiXml, "takeall_button", this);
-	m_sleep_button		= UIHelper::Create3tButtonEx(uiXml, "sleep_button", this);
 	m_exit_button		= UIHelper::Create3tButtonEx(uiXml, "exit_button", this);
+	m_sleep_button		= UIHelper::Create3tButtonEx(uiXml, "sleep_button", this);
 
 	m_clock_value						= UIHelper::CreateStatic(uiXml, "clock_value", this);
 
@@ -296,17 +296,16 @@ void CUIActorMenu::InitCallbacks()
 {
 	Register( m_trade_button );
 	Register( m_takeall_button );
-
-	Register( m_sleep_button );
 	Register( m_exit_button );
+	Register( m_sleep_button );
 	Register( m_UIPropertiesBox );
 	VERIFY( m_pUpgradeWnd );
 	Register( m_pUpgradeWnd->m_btn_repair );
 
 	AddCallback( m_trade_button->WindowName(),    BUTTON_CLICKED,   CUIWndCallback::void_function( this, &CUIActorMenu::OnBtnPerformTrade ) );
 	AddCallback( m_takeall_button->WindowName(),  BUTTON_CLICKED,   CUIWndCallback::void_function( this, &CUIActorMenu::TakeAllFromPartner ) );
-	AddCallback( m_sleep_button->WindowName(),    BUTTON_CLICKED,   CUIWndCallback::void_function( this, &CUIActorMenu::OnBtnSleepClicked ) );
 	AddCallback( m_exit_button->WindowName(),     BUTTON_CLICKED,   CUIWndCallback::void_function( this, &CUIActorMenu::OnBtnExitClicked ) );
+	AddCallback( m_sleep_button->WindowName(),    BUTTON_CLICKED,   CUIWndCallback::void_function( this, &CUIActorMenu::OnBtnSleepClicked ) );
 	AddCallback( m_UIPropertiesBox->WindowName(), PROPERTY_CLICKED, CUIWndCallback::void_function( this, &CUIActorMenu::ProcessPropertiesBoxClicked ) );
 	AddCallback( m_pUpgradeWnd->m_btn_repair->WindowName(), BUTTON_CLICKED,   CUIWndCallback::void_function( this, &CUIActorMenu::TryRepairItem ) );
 }
