e14c8f096e624fe2ada831683d7025f5af7d34f3
noxiossy <cari0us@outlook.com>
2022-06-20 20:34:53 +0700

    hghg

--- xray/Layers/xrRender/DetailManager.cpp
+++ xray/Layers/xrRender/DetailManager.cpp
@@ -401,7 +401,7 @@ void __stdcall	CDetailManager::MT_CALC		()
 	if (!RImplementation.Details) return;	// possibly deleted
 	if (!dtFS) return;
 	if (!psDeviceFlags.is(rsDetails)) return;
-	if (g_pGamePersistent && g_pGamePersistent->m_pMainMenu && g_pGamePersistent->m_pMainMenu->IsActive()) return;
+	//if (g_pGamePersistent && g_pGamePersistent->m_pMainMenu && g_pGamePersistent->m_pMainMenu->IsActive()) return;
 #endif    
 
 	MT.Enter					();
